7a9fd892c4d1ac8131b2dd11ebea1ece6d2445ff
Hans Findel <hansfindel@Hanss-MacBook-Pro.local>
2012-02-07 14:39:49 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaGit::Application.config.secret_token = 'df3919e2ff78290aced691578043872d3d4b75ef1b39a26288c91254e139b210a60312c334e05856a5a5a49ce8f109e8277d1db6f749851615be08258eb27a39'
